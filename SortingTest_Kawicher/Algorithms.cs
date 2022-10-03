85fedd5cda7593706287d01d258bb4c0bee0b438
DuSack1220 <sebastiankawicher@gmail.com>
2022-10-03 14:04:39 +0200

    bissl heap aber ohne sort

---
+++ SortingTest_Kawicher/Algorithms.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Class1
+{
+	public Class1()
+	{
+		int[] array = {10, 6, 4, 8, 2};
+
+		public void QuickSort()
+        {
+			Console.WriteLine("ich will sterben");
+        }
+
+		private void QuickSort(int start, int end)
+        {
+
+        }
+
+		public void HeapSort()
+        {
+			HeapSort(0);
+        }
+
+		private void HeapSort(int sorted)
+        {
+			for (int i = (array.Lenght - sortd -1) / 2; i > 0; i--)
+            {
+				if (array[i * 2 + 1] > array[i])
+                {
+                    array[i], array[i * 2 + 1] = array[i * 2 + 1], array[i];
+                }
+            }
+            array[0], array[array.Lenght - 1] = array[array.Lenght - 1],  array[0];
+            HeapSort(sorted + 1);
+        }
+	}
+}
