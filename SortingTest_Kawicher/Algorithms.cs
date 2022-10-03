8c2a8ad16ca43dadcfb6c276bb8452e1d46bd14c
DuSack1220 <sebastiankawicher@gmail.com>
2022-10-03 23:48:17 +0200

    added old shit

--- SortingTest_Kawicher/Algorithms.cs
+++ SortingTest_Kawicher/Algorithms.cs
@@ -7,9 +7,52 @@ public class Class1
 {
 	public Class1()
 	{
-		int[] array = {10, 6, 4, 8, 2};
 
-		public void QuickSort()
+        private int[] array;
+
+        int[] CreateRandoms(int min, int max, int count, bool duplicates)
+        {
+            Random rand = new Random();
+            if (max - min < 1 || max - min < count)
+                return null;
+
+            array = new int[count];
+            while (array.Length < count)
+            {
+                int newInt = rand.Next(min, max);
+                if (array.Contains(newInt) && !duplicates)
+                    continue;
+                array.Append(newInt);
+            }
+
+            return array;
+        }
+
+        void Print()
+        {
+            foreach (var VARIABLE in array)
+            {
+
+            }
+        }
+
+        void SetRandoms(int[] a)
+        {
+            array = new int[a.Length];
+            a.CopyTo(array, 0);
+        }
+
+        int[] GetRandoms()
+        {
+            return array;
+        }
+
+        void BubbleSort()
+        {
+
+        }
+
+        public void QuickSort()
         {
 			Console.WriteLine("ich will sterben");
         }
