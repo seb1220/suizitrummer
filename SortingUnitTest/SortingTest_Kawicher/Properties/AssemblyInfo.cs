4f4d6f8ab19ff75b0e628c79f06d6e2a2c3bfafe
DuSack1220 <sebastiankawicher@gmail.com>
2022-10-19 15:08:51 +0200

    renamed Project -> Kawicher (with one r at the end)

--- SortingUnitTest/SortingTest_Kawicher/Properties/AssemblyInfo.cs
+++ SortingUnitTest/SortingTest_Kawicher/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("SortingTest_Kawicherr")]
+[assembly: AssemblyTitle("SortingTest_Kawicher")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SortingTest_Kawicherr")]
+[assembly: AssemblyProduct("SortingTest_Kawicher")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
